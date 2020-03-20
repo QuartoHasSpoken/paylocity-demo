4bbc479d354e3326c29ec416dc2ed2f354ac93b6
beauXjames <beauxjames@gmail.com>
2020-03-20 00:10:44 -0500

    Updated Benefits Spec

--- src/app/benefits.service.spec.ts
+++ src/app/benefits.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { BenefitsService } from './benefits.service';
+import {Employee} from './employee.service';
 
 describe('BenefitsService', () => {
   let service: BenefitsService;
@@ -21,5 +22,71 @@ describe('BenefitsService', () => {
     expect(total).toBe(100);
     total = service.applyNameDiscount('Abigail', discount, total);
     expect(total).toBe(90);
+  });
+
+  it('should calculate employee with no dependents no discounts', () => {
+      let employee: Employee;
+      employee = {
+          id: 1,
+          firstName: 'John',
+          lastName: 'Doe',
+          dependents: []
+        };
+      const total = service.calculateEmployee(employee);
+      expect(total).toBe(1000);
+  });
+
+  it('should calculate employee with no dependents name discount', () => {
+    let employee: Employee;
+    employee = {
+      id: 1,
+      firstName: 'Abbey',
+      lastName: 'Doe',
+      dependents: []
+    };
+    const total = service.calculateEmployee(employee);
+    expect(total).toBe(900);
+  });
+
+  it('should calculate employee with 1 dependent no discounts', () => {
+    let employee: Employee;
+    employee = {
+      id: 1,
+      firstName: 'John',
+      lastName: 'Doe',
+      dependents: ['Mickey']
+    };
+    const total = service.calculateEmployee(employee);
+    expect(total).toBe(1500);
+  });
+
+  it('should calculate employee with 1 dependent name discount', () => {
+    let employee: Employee;
+    employee = {
+      id: 1,
+      firstName: 'John',
+      lastName: 'Doe',
+      dependents: ['Abigail']
+    };
+    let total = service.calculateEmployee(employee);
+    expect(total).toBe(1450);
+    employee.firstName = 'Amy';
+    total = service.calculateEmployee(employee);
+    expect(total).toBe(1350);
+  });
+
+  it('should calculate employee with multiple dependents mixed discounts', () => {
+    let employee: Employee;
+    employee = {
+      id: 1,
+      firstName: 'John',
+      lastName: 'Doe',
+      dependents: ['Abigail', 'Fran', 'Poppy']
+    };
+    let total = service.calculateEmployee(employee);
+    expect(total).toBe(2450);
+    employee.firstName = 'Amy';
+    total = service.calculateEmployee(employee);
+    expect(total).toBe(2350);
   });
 });
